0041f3b088da0b0d5d3edebc39964aeff4693a8b
Colin Tindle <ampersand@underscore.space>
2018-02-12 23:16:53 +0000

    Desktop has some icons Default system fonts now used

--- app/desktop.js
+++ app/desktop.js
@@ -1,7 +1,59 @@
+import {v4 as uuid} from 'uuid'
+
+import APP_ICON from './icons/app.png'
+import FILE_ICON from './icons/file.png'
+
+const ICONS = [APP_ICON, FILE_ICON]
+const ICON_TEMPLATE = document.getElementById('icon-template').querySelector('.icon')
+
+class Icon {
+  constructor(os, {title, type, window}) {
+    this.os = os
+    this.icon = ICON_TEMPLATE.cloneNode(true)
+    this.id = uuid()
+
+    this.title = this.icon.querySelector('.icon__title')
+    this.image = this.icon.querySelector('.icon__image')
+
+    this.title.innerText = title
+    this.image.style.backgroundImage = `url(${ICONS[type]})`
+    this.icon.addEventListener('dblclick', () => this.load(window))
+    this.icon.addEventListener('click', e => this.select(e))
+  }
+
+  load(window) {
+    this.os.openWindow(window, this.id)
+  }
+
+  select() {
+    if (this.selected) {
+      this.deselect()
+      return
+    }
+
+    this.os.desktop.deselectAllIcons()
+    this.selected = true
+    this.icon.classList.add('selected')
+    this.icon.style.setProperty('--text-color', 'var(--atari-white)')
+    this.icon.style.setProperty('--icon-title', 'black')
+  }
+
+  deselect() {
+    this.selected = false
+    this.icon.classList.remove('selected')
+    this.icon.style.setProperty('--text-color', 'black')
+    this.icon.style.setProperty('--icon-title', 'var(--atari-white)')
+  }
+}
+
 export default class Desktop {
   constructor(os) {
     this.os = os
+    this.icons = []
+
     this.desktop = document.getElementById('desktop')
+
+    this.desktop.addEventListener('click', e => this.deselectAllIcons(e))
   }
 
   pos() {
@@ -11,5 +63,16 @@ export default class Desktop {
 
   addWindow(win) {
     this.desktop.appendChild(win.window)
+  }
+
+  addIcon(icon) {
+    const newIcon = new Icon(this.os, icon)
+    this.icons.push(newIcon)
+    this.desktop.appendChild(newIcon.icon)
+  }
+
+  deselectAllIcons(e) {
+    if (e && e.target.id !== 'desktop') {return}
+    this.icons.forEach(icon => icon.deselect())
   }
 }
--- app/index.js
+++ app/index.js
@@ -1,6 +1,12 @@
 import tos from './os'
 window.os = new tos
 
-window.os.openWindow({page:'https://emoji.messy.cloud', title:'Emojis', w:400, h:345, y:10})
-window.os.openWindow({page:'https://router.messy.cloud', title:'neeko-router', w:320, h:200, y:365})
-window.os.openWindow({page:'https://screensaver.messy.cloud', title:'Mac Plus Screensaver', w:240, h:200, x: 10, y:100})
+os.addIcon({title:'emojis.htm', type:os.icon.file, window: {
+  page:'https://emoji.messy.cloud', title:'[CAB] Emojis', w:400, h:345, y:10
+}})
+os.addIcon({title:'scrsaver.app', type:os.icon.app, window: {
+  page:'https://screensaver.messy.cloud', title:'Mac Plus Screensaver', w:240, h:200, x: 10, y:100
+}})
+os.addIcon({title:'router.htm', type:os.icon.file, window: {
+  page:'https://router.messy.cloud', title:'[CAB] neeko-router Demo', w:320, h:200, y:365
+}})
