8d774adfa699cb4f6e04edb7da7fdb8d67ff4d15
Colin Tindle <underscoredotspace@users.noreply.github.com>
2018-02-25 11:58:15 +0000

    Update emojis window size (#12)

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import tos from './os/os'
 window.os = new tos
 
 os.addIcon({title:'emojis.htm', type:os.icon.file, window: {
-  page:'https://emoji.messy.cloud', title:'[CAB] Emojis', w:400, h:345, y:10
+  page:'https://emoji.messy.cloud', title:'[CAB] Emojis', w:386, h:330, y:10
 }})
 os.addIcon({title:'scrsaver.app', type:os.icon.app, window: {
   page:'https://screensaver.messy.cloud', title:'Mac Plus Screensaver', w:240, h:200, x: 10, y:100
