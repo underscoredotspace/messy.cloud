4f830f8502109b84781ad4834c5b42c70966f35b
Colin Tindle <ampersand@underscore.space>
2018-02-27 22:21:14 +0000

    Removed #mobile from winter-scene

--- app/index.js
+++ app/index.js
@@ -8,7 +8,7 @@ os.addIcon({title:'scrsaver.app', type:os.icon.app, window: {
   page:'https://screensaver.messy.cloud', title:'Mac Plus Screensaver', w:240, h:200, x: 10, y:100
 }})
 os.addIcon({title:'winter.app', type:os.icon.app, window: {
-  page: 'https://winter.messy.cloud/#mobile', title: 'Winter Scene', w:415, h:500
+  page: 'https://winter.messy.cloud', title: 'Winter Scene', w:415, h:500
 }})
 os.addIcon({title:'router.htm', type:os.icon.file, window: {
   page:'https://router.messy.cloud', title:'[CAB] neeko-router Demo', w:320, h:200, y:365
